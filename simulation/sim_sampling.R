f59a663807e76a9708d5d3d020b98ed01b8dcd68
Hang Wu <wuhang997@gmail.com>
2026-01-31 14:48:00 -0500

    simulation_test
    update sim_sampling.R and sim_scs.R.
    sim_sampling.R: add comment
    sim_scs.R. : add code to create output folder and store results

--- simulation/sim_sampling.R
+++ simulation/sim_sampling.R
@@ -2,10 +2,14 @@
 ## Replication Materials
 
 
-# This scripts generate simulated samples
+# This scripts generate simulated samples for different cases
+# One case is defined by a combination of N (number of units) and T0 (number of pre-treatment periods)
 
+# %% set environment %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+rm(list=ls(all=TRUE))
 
-setwd("/Users/wuhang/Desktop/metric/sc/script/simulation")
+set.seed(123)
+setwd("/Users/wuhang/Desktop/metric/sc/replication_files/simulation")
 library(R.matlab)
 library(doParallel)
 library(foreach)
@@ -13,23 +17,25 @@ library(MASS)
 source("functions/sampling.R")
 
 # %% pre-set parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-sims<-100
+sims<-100 # number of simulations per case
 
-TT0 <- c(50)
-NN  <- c(20)
-S <- 10
-# Create all combinations of TT0 and NN
-cases <- expand.grid(TT0 = TT0, NN = NN)
+# define cases
+TT0 <- c(50) # vector of target pre-treatment periods
+NN  <- c(20) # vector of target units
+S <- 10 # number of post-treatment periods
+cases <- expand.grid(TT0 = TT0, NN = NN) # Create all combinations of TT0 and NN
 ncases <- nrow(cases)
 
-r <- 2
-p <- 0
+# parameters for DGP
+r <- 2 # number of latent factors
+FE <- TRUE # include fixed effects
+p <- 0 # number of observed covariates
+AR1 <- 0.5 # AR(1) coefficient for latent factors and time fixed effects
 
 
 
 # %% Generate simulated data %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-set.seed(123)
 # register multiple cores
 cores<-8
 Sys.setenv(GOTO_NUM_THREADS=cores)
@@ -38,8 +44,8 @@ registerDoParallel(cl)
 cat("Cores: ", cores, "; cases: ", ncases, "\n", sep = "")
 begin.time <- Sys.time()
 # start simulation
-results_list <- list()
-results_list_mat <- list()  # for MATLAB-compatible structure
+sim_data <- list()
+sim_data_mat <- list()  # for MATLAB-compatible structure
 for (case in 1:ncases) {
     N <- cases$NN[case]
     T0 <- cases$TT0[case]
@@ -49,12 +55,12 @@ for (case in 1:ncases) {
         ", N = ", N, ", T0 = ", T0, ", S = ", S, "\n", sep = "")
     
     # generate treatment matrix
-    repeat { # randomly generate A matrix until at least some units are treated
+    repeat { # randomly generate A matrix until at least r+1 units are treated, for feasibility of gyc
         A = get_treatment_matrix(N=N, S=S, r=2)
-        r_treated = mean(colSums(A) > 0)
-        if (r_treated > 0) break
+        N_treated = sum(colSums(A) > 0)
+        if (N-N_treated >= r+1) break
     }
-    cat("Treated units proportion: ", r_treated, "\n", sep = "")
+    cat("Treated units proportion: ", N_treated / N, "\n", sep = "")
     
     # simulate data in parallel
     onecase <- foreach(i = 1:sims,
@@ -64,14 +70,14 @@ for (case in 1:ncases) {
                        .packages = c("MASS")
     ) %dopar% {
         # generate a random sample: 
-        panel <- simulate_data_fm(N = N, T0 = T0, S = S, p = p, r = r,
+        panel <- simulate_data_fm(N = N, T0 = T0, S = S, p = p, r = r, AR1 = AR1,
                           D.sd = 0, te = NULL,
                           A = A,
                           mu = 5,
-                          FE = TRUE)
+                          FE = FE)
         list(panel)
     }
-    results_list[[case_name]] <- onecase
+    sim_data[[case_name]] <- onecase
 
     # Convert simulation result to MATLAB-style arrays
     onecase_list <- lapply(onecase, function(df) {
@@ -87,10 +93,10 @@ for (case in 1:ncases) {
     for (i in seq_along(onecase_list)) {
       onecase_array[,,i] <- onecase_list[[i]]
     }
-    results_list_mat[[case_name]] <- onecase_array
+    sim_data_mat[[case_name]] <- onecase_array
 
     # Save RData intermediate
-    # save(results_list, file = "sim_data.RData") # store intermediate results to avoid loss by crashes 
+    save(sim_data, file = "sim_data.RData") # store intermediate results to avoid loss by crashes 
 }
 stopCluster(cl) # stop parallel computing
 
@@ -98,70 +104,17 @@ time<-Sys.time()-begin.time
 print(time)
 
 # %%Save simulated data %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# check structure of saved data
+# check structure of simulated data
 if (interactive()) {
-  cat("Number of cases:", length(results_list), "\n")
-  cat("Case names:", paste(names(results_list), collapse = ", "), "\n")
-  cat("Number of simulations in first case:", length(results_list[[1]]), "\n")
+  cat("Number of cases:", length(sim_data), "\n")
+  cat("Case names:", paste(names(sim_data), collapse = ", "), "\n")
+  cat("Number of simulations in first case:", length(sim_data[[1]]), "\n")
   cat("First 5 rows of first simulated panel (first case, first sim):\n")
-  print(print(head(results_list[[1]][[1]], 2)))
+  print(print(head(sim_data[[1]][[1]], 2)))
 }
-a = results_list[[1]] # list of sims
-a = results_list[[1]][[1]] # dataframe of one sim
 
 # save simulated data
-save(results_list,file="data/sim_data.RData")
-do.call(writeMat, c(list("data/sim_data.mat"), results_list_mat))
+save(sim_data,file="data/sim_data.RData")
+do.call(writeMat, c(list("data/sim_data.mat"), sim_data_mat))
 
 print("Simulation data saved to 'sim_data.RData' and 'sim_data.mat'.")
-
-#  %%%%%%% archive%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# Convert results_list to MATLAB-style arrays
-load("sim_data.RData")
-results_list_mat <- list()  # for MATLAB-compatible structure
-# convert
-results_list_mat <- foreach(case_name = names(results_list), 
-                            .combine = 'list',
-                            .multicombine = TRUE,
-                            .packages = c(),
-                            .export = c()) %do% {  # use %dopar% if you want parallel
-  cat("Processing case: ", case_name, "\n", sep = "")
-  onecase <- results_list[[case_name]]
-  # Convert each simulation to a numeric matrix
-    onecase_list <- lapply(onecase, function(df) {
-    required_cols <- c('id', 'time', 'Y', 'D','att') # remain only id, time, D, Y for matlab
-    df_sel <- df[, required_cols]
-    df_sel[] <- lapply(df_sel, as.numeric)
-    as.matrix(df_sel)
-  })
-
-  # Build 3D array
-  rows_NT <- nrow(onecase_list[[1]])
-  num_col <- ncol(onecase_list[[1]])
-  num_rep <- length(onecase_list)
-  onecase_array <- array(NA, dim = c(rows_NT, num_col, num_rep))
-  for (i in seq_along(onecase_list)) {
-    onecase_array[,,i] <- onecase_list[[i]]
-  }
-
-  onecase_array
-}
-names(results_list_mat) <- names(results_list) # Set the names of the list to match your cases
-a = results_list_mat[[1]] # array of one case: rows x cols x sims
-
-# A test for parallel simulation
-sims <- 500
-cores<-8
-Sys.setenv(GOTO_NUM_THREADS=cores)
-cl<-makeCluster(cores)
-registerDoParallel(cl)
-onecase <- foreach(i = 1:sims,
-                   .combine = 'c',          # combine lists
-                   .multicombine = TRUE,    # allow multicombine
-                   .packages = c("MASS")) %dopar% {
-  # simulate a dataframe
-  df <- data.frame(x = rnorm(5), y = runif(5))
-  
-  list(df)  # wrap in list so c() combines correctly
-}
-a = onecase[[1]] # first dataframe
--- simulation/sim_scs.m
+++ simulation/sim_scs.m
@@ -9,31 +9,23 @@ alpha_sig = .05;
 
 
 
-%% DATA CLEANING
-% data = readtable('simulated_data.csv');
+%% LOAD DATA
 data = load('sim_data.mat');
-field_names = fieldnames(data);
-% data = data(:, {'id','time','D','Y'});
 
 %% PREPARATION
-% field_names = field_names{1};
-% Initialize result structs to store each NT setting
+case_names = fieldnames(data);
 ATT_struct = struct();
 ATT_event_struct = struct();
 
-%% LOOP
-for f = 1:numel(field_names)
+%% START SIMULATION
+for f = 1:numel(case_names)
 
-    field = field_names{f};
+    case_name = case_names{f};
+    data_case = data.(case_name);   % each data_case contains one N-T combination
+    num_sims = size(data_case, 3);     % # of simulation
+    fprintf('Processing case: %s, nsims = %d\n', case_name, num_sims);
 
-
-    data_field = data.(field);   % each data_field contains one N-T combination
-    
-    num_sims = size(data_field, 3);     % # of simulation
-    fprintf('Processing case: %s, nsims = %d\n', field, num_sims);
-
-    % Prepare storage for this field
-%     ATT = cell(num_sims,1);
+    % Prepare storage for this case
     ATT_event = cell(num_sims,1);
     
     %%PARALLEL LOOP OVER SIMULATIONS
@@ -46,7 +38,7 @@ for f = 1:numel(field_names)
         end
 
         %%Extract simulation i
-        data_i = data_field(:,:,i);
+        data_i = data_case(:,:,i);
         
         eff = (1:10)';  % true event-tim effect 
         att = unique(data_i(:,5));  % true ATT
@@ -65,266 +57,55 @@ for f = 1:numel(field_names)
         D = D(:,1:T1);
         D_S = D(:,T+1:T+S);
         
-        %%ESTIMATION
-%         output = att_ci(Y,D,S,alpha_sig);
-%         att_hat = output.att_hat;
-%         
+        %%ESTIMATION         
         output_t = att_event_ci(Y,D,S,alpha_sig);
         att_hat_t = output_t.att_hat;
         
         %%CONFIDENCE INTERVALS
-%         ci_l = att_hat - output.ub;
-%         ci_u = att_hat - output.lb;
         ci_l_t = att_hat_t - output_t.ub;
         ci_u_t = att_hat_t - output_t.lb;
         
         %%COLLECT RESULTS
-%       ATT{i} = [att, att_hat, att_hat-att, (att_hat-att).^2, ci_l, ci_u, (ci_l<=att) & (ci_u>=att)];
-%       ATT_event{i} = [(1:S)', eff(1:S), att_hat_t, att_hat_t - eff(1:S), (att_hat_t-eff(1:S)).^2, ci_l_t, ci_u_t, (ci_l_t<=eff(1:S)) & (ci_u_t>=eff(1:S))];
-%       ATT{i} = [att_hat, att_hat-att, (att_hat-att).^2, (ci_l<=att) & (ci_u>=att)];
         ATT_event{i} = [(1:S)', att_hat_t - eff(1:S), att_hat_t, (att_hat_t-eff(1:S)).^2, (ci_l_t<=eff(1:S)) & (ci_u_t>=eff(1:S))];
         
+        %%USED TIME PER SIM
         elapsed_time(i,1) = toc(t0);  
         fprintf('Sim %d | elapsed %.2f sec\n', ...
             i,  elapsed_time(i,1));
     end
     
     %%STORE RESULTS IN STRUCT
-%     ATT_struct.(field) = ATT;
-    ATT_event_struct.(field) = ATT_event;
+    ATT_event_struct.(case_name) = ATT_event;
 end
 disp('Finished')
 %%
 save('sim_result_SSC.mat', 'ATT_struct', 'ATT_event_struct')
 
-%% 
+%% SUMMARY
+%%extract results
 sim_mat = fieldnames(ATT_event_struct);
 sim_mat = sim_mat{1};
 sim_mat = ATT_event_struct.(sim_mat)(cellfun(@(x) size(x,1) == 10, ATT_event_struct.(sim_mat)));
 sim_mat = cat(3, sim_mat{:});
 
-ATT = squeeze(sim_mat(:, 3, :));   % N × sims
+%%summary
+ATT = squeeze(sim_mat(:, 3, :));
 ATT_Var = var(ATT, 0, 2); 
 
 
 elapsed_time = repmat(sum(elapsed_time), length(ATT_Var), 1);
-%%
-a = mean(sim_mat, 3);
-a(:,3) = ATT_Var;    
-a = [a, elapsed_time];
-a = num2cell(a);
-a = cell2table(a, 'VariableNames', ...
-    {'event_time','bias','var', 'se','coverage_rate', 'time_total'});
-%%
-a = ATT_event_struct.N50_T050(cellfun(@(x) size(x,1) == 10, ATT_event_struct.N50_T050));
-a = cat(3, a{:});
-a = mean(a, 3);
 
-%% test
-data = data.N15_T0100(:,:,3);
-N = length(unique(data(:,1))); % number of units
-T1 = length(unique(data(:,2))); % number of times
-Y = reshape(data(:,3),T1,N)'; 
-D = reshape(data(:,4),T1,N)'; 
-%%
-T = find(sum(D),1)-1; % number of pre-treatment periods
-S_max = T1-T; % maximum number of post-treatment periods
-% S = min(S_max,round(T/2)); % truncate data to avoid extrapolating too far
-S = S_max;
-T1 = T+S; % total number of periods
-Y = Y(:,1:T1); % outcome 
-D = D(:,1:T1); % all-time treatment status
-D_S = D(:,T+1:T+S); % post-treatment treatment status
-%% number of treated units
-a = sum(D_S,2);
-a = length(find(a));
-
-%% ESTIMATION
-
-
-output = att_ci(Y,D,S,alpha_sig);
-att_hat = output.att_hat;
-ub = output.ub;
-lb = output.lb;
-
-
-output_t = att_event_ci(Y,D,S,alpha_sig);
-att_hat_t = output_t.att_hat;
-ub_t = output_t.ub;
-lb_t = output_t.lb;
-
-
-
-%% OUTPUT
-
-% % treatment effects estimates (starting from T+1)
-% te_mat_hat 
-
-% event time ATT estimates and confidence intervals
-disp('[event time, att estimate, confidence interval]')
-disp([(1:S)',att_hat_t,att_hat_t-ub,att_hat_t-lb])
-
-
-% ATT estimates and confidence intervals
-disp('[att estimate, confidence interval]')
-disp([att_hat,att_hat-ub,att_hat-lb])
-
-
-
-
-%% event time ATT plot
-figure
-p1 = errorbar(1:S,att_hat_t,ub_t,-lb_t,'CapSize',10);
-hold on 
-plot(-10:0.1:10,-10:0.1:10,'--k')
-plot([.5,S+.5],[0,0],'--k')
-hold off
-p1.Marker = 'o';
-p1.MarkerSize = 6;
-p1.LineWidth = 2;
-xlabel('event time','FontSize',15)
-ylabel('ATT estimates','FontSize',15)
-xlim([.5 S+.5])
-%% ATT plot
-%orginal results in the orginal paper
-beta = 0.941;
-sd = 0.528;
-z = 1.645;   % 90% CI
-ci_low = beta - z*sd;
-ci_high = beta + z*sd;
-
-
-figure
-hold on 
-p1 = errorbar(S/3,att_hat,ub,-lb,'CapSize',10);
-p2 = errorbar(2*S/3,beta,z*sd,'CapSize',10);
-plot([.5,S+.5],[0,0],'--k')
-hold off
-p1.Marker = 'o';
-p1.MarkerSize = 6;
-p1.LineWidth = 2;
-p2.Marker = 'o';
-p2.MarkerSize = 6;
-p2.LineWidth = 2;
-ylabel('ATT estimates','FontSize',15)
-ax = gca;
-ax.XColor = 'none';    % makes the x-axis line invisible
-legend([p1 p2], {'SSC', 'Original:General synthetic control (Xu, 2017)'}, ...
-       'Location', 'northwest', ...
-       'FontSize', 8);
-
-
-%% residuals/treatment effects plot
-
-% residuals/treatment effects of ever-treated units plot
-y_min = min(min(res_mat));
-y_max = max(max(res_mat));
-
-figure
-subplot(2,1,1)
-p = plot(res_mat','LineWidth',2);
-hold on 
-plot([0,T+S+1],[0,0],'--k')
-vline(T,'--k');
-hold off
-xlim([1,T+S])
-ylim([y_min,y_max])
-xlabel('time','FontSize',15)
-ylabel('treatment effects','FontSize',15)
-title('Calendar Time','FontSize',15)
-
-
-% residuals/treatment effects of ever-treated units plot (recentered at 
-% first treatment time)
-subplot(2,1,2)
-x_min = min(min(event_time_mat));
-x_max = max(max(event_time_mat));
-plot([x_min-1,x_max+1],[0,0],'--k')
-hold on
-for i = 1 : size(res_mat,1)
-    plot(event_time_mat(i,:),res_mat(i,:),'LineWidth',2)
+summary = mean(sim_mat, 3);
+summary(:,3) = ATT_Var;    
+summary = [summary, elapsed_time];
+summary = num2cell(summary);
+summary = cell2table(summary, 'VariableNames', ...
+    {'event_time','bias','var', 'mse','coverage_rate', 'time_total(s)'});
+%% SAVE
+if ~exist('output', 'dir')
+    mkdir('output')
 end
-vline(0,'--k');
-hold off
-x_min_plot = -5; % left limit of plot 
-xlim([x_min_plot, x_max])
-ylim([y_min,y_max])
-xlabel('time relative to first treated','FontSize',15)
-ylabel('treament effects','FontSize',15)
-title('Event Time','FontSize',15)
-
-
-
-%% results by other methods
-gsyn = readtable('gsyn.csv');
-gsyn.ub = gsyn.ATT - gsyn.CI_lower;
-gsyn.lb = gsyn.ATT - gsyn.CI_upper;
-
-asyn = readtable('asyn.csv');
-asyn = asyn(asyn.Time>=0,:);
-asyn.ub = asyn.Estimate - asyn.lower_bound;
-asyn.lb = asyn.Estimate - asyn.upper_bound;
-
-did = readtable('did.csv');
-did.ub = did.att - did.CI_lower;
-did.lb = did.att - did.CI_upper;
-
-%% plot1 - absolute level treatment effects for two policies
-figure(1)
-hold on
-
-% --- SSC (coral-red) ---
-p1 = errorbar(1:S, att_hat_t, ub_t, -lb_t, ...
-    'Color',[1,0.4,0.3],'CapSize',10); % errorbar 1
-p1.Marker = 'o'; p1.LineStyle = 'none'; p1.LineWidth = 2;
-
-p2 = plot(1:S, att_hat_t, 'Color',[1,0.4,0.3],'LineWidth',2); % line 2
-
-% --- GSC (teal) ---
-p3 = errorbar(1:S, gsyn.ATT, gsyn.ub, -gsyn.lb, ...
-    'Color',[0,0.6,0.6],'CapSize',10); % errorbar 3
-p3.Marker = 'o'; p3.LineStyle = 'none'; p3.LineWidth = 2;
-
-p4 = plot(1:S, gsyn.ATT, '--', 'Color',[0,0.6,0.6],'LineWidth',2); % line 4
-
-% --- ASC (green) ---
-p5 = errorbar(1:size(asyn,1), asyn.Estimate, asyn.ub, -asyn.lb, ...
-    'Color',[0.3,0.6,0.2],'CapSize',10); % errorbar 5
-p5.Marker = 'o'; p5.LineStyle = 'none'; p5.LineWidth = 2;
-
-p6 = plot(1:size(asyn,1), asyn.Estimate, ':', 'Color',[0.3,0.6,0.2],'LineWidth',2); % line 6
-
-% --- DID (purple) ---
-p7 = errorbar(1:S, did.att, did.ub, -did.lb, ...
-    'Color',[0.6,0.4,0.8],'CapSize',10); % errorbar 7
-p7.Marker = 's'; p7.LineStyle = 'none'; p7.LineWidth = 2;
-
-p8 = plot(1:S, did.att, '-.', 'Color',[0.6,0.4,0.8],'LineWidth',2); % line 8
-
-% --- Axes & labels ---
-xlim([0.5, S+0.5])
-xlabel('event time','FontSize',15)
-ylabel('ATT estimates','FontSize',15)
-
-% --- Legend (reference only line plots) ---
-lgd = legend([p2, p4, p6, p8], ...
-    {'Staggered synthetic control', ...
-     'Staggered diff-in-diff (Callaway and SantAnna, 2018)', ...
-     'Augmented synthetic control', ...
-     'DID-ASC'}, ...
-    'Location','northwest');
-lgd.FontSize = 12;
-
-hold off
-
-%%
-% save plots
-saveas(figure(1),'temp/att_comparison.png')
-
-
-
-
-
+writetable(summary, 'output/sim_summary_SSC.csv');
 
 
 
